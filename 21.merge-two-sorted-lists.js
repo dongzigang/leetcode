392b42fdbb7fe618267b36965c3ddeba0fc9ed80
dongzigang <dongzigang@bailuntec.com>
2021-02-27 18:44:06 +0800

    update

---
+++ 21.merge-two-sorted-lists.js
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+
+};
+
+let l1 = [1,2,4], l2 = [1,3,4]
+
+console.log(mergeTwoLists(l1,l2))
