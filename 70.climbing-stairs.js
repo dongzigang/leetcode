2e747e1e9144956aff170ef4c99641d1f9753993
不语 <821220992@qq.com>
2021-03-23 11:57:56 +0800

    Update 70.climbing-stairs.js
    update

--- 70.climbing-stairs.js
+++ 70.climbing-stairs.js
@@ -4,5 +4,24 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-
+  let memo = new Array(n + 1)
+  return climbStairsMemo(n, memo)
 };
+
+function climbStairsMemo(n, memo) {
+  if(memo[n] > 0){
+    return memo[n]
+  }
+  if(n === 1){
+    memo[n] = 1
+  }
+  else if( n === 2){
+    memo[n] = 2
+  }
+  else {
+    memo[n] = climbStairsMemo(n - 1, memo) + climbStairsMemo(n - 2, memo)
+  }
+  return  memo[n]
+}
+
+console.log(climbStairs(4))
