a244bbac1dbadcd26e9e51389b63a696e616881d
dongzigang <dongzigang@bailuntec.com>
2021-04-17 15:07:50 +0800

    update

--- 21.merge-two-sorted-lists.js
+++ 21.merge-two-sorted-lists.js
@@ -12,9 +12,27 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
+  // 使用带头结点的链表解决问题
+  // 待输出链表的头部
+  let head = new ListNode();
 
+  // 待输出链表的 last 结点
+  let last = head;
+  while(l1 != null && l2 != null) {
+    if(l1.val > l2.val) {
+      last.next = l2;
+      l2 = l2.next;
+    }else{
+      last.next = l1;
+      l1 = l1.next;
+    }
+    last = last.next;
+  }
+
+  // l1 或 l2 可能还有剩余结点没有合并，
+  // 由于从上面的 while 循环中退出， 那么链表 l1 和 l2 至少有一个已经遍历结束
+  if(l1 != null) last.next = l1;
+  if(l2 != null) last.next = l2;
+
+  return head.next;
 };
-
-let l1 = [1,2,4], l2 = [1,3,4]
-
-console.log(mergeTwoLists(l1,l2))
